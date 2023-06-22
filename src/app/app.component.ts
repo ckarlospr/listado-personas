c5d0f2fc0ddc04ce42015188cb17f42cf1405800
Carlos Perez <per.ckarlos@gmail.com>
2023-06-21 22:52:15 -0500

    adding new person class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de personas';
+  personas: Persona[] = [new Persona('Carlos', 'Perez'), new Persona('Juan', 'Castro')];
 }
---
+++ src/app/persona.model.ts
@@ -0,0 +1,15 @@
+export class Persona{
+    /*
+    nombre: string =  '';
+    apelido: string =  '';
+
+    constructor(string nombre, string apellido){
+        this.nombre = nombre;
+        this.apelido = apellido;
+    }
+    */
+
+    constructor( public nombre: string, public apellido: string ){
+        
+    }
+}
